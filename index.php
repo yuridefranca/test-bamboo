a5ecfaaed03439173be000d4f8f97b1cbb9c7554
Yuri Serafim <yuridefranca@gmail.com>
2024-03-11 23:50:11 +0000

    refactor: remove useless comments from the source code

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-
-// The Task
-// Develop a script to build a company tree with associated travel cost from two API endpoints
-
-// Company List: https://5f27781bf5d27e001612e057.mockapi.io/webprovise/companies
-// Travel List: https://5f27781bf5d27e001612e057.mockapi.io/webprovise/travels
-
-
-// Requirements:
-
-// Process data from the two endpoints to have a nested array of companies with travel cost
-// Travel cost of a particular company is the total travel price of employees in that company and its child companies
-
-// Usage: php index.php
-
 class RestWrapper
 {
   public static function request(string $url, string $method, $formatter = null)
